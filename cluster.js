d05710069783077aa2f1336c5a26817fc2b8251e
Khalid Shaikh <k@ruby.run>
2019-04-29 02:37:26 +0000

    Initial commit

---
+++ cluster.js
@@ -0,0 +1,22 @@
+#!/usr/local/bin/node
+// https://nodejs.org/api/cluster.html
+
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+// console.log(numCPUs)
+
+console.log(`pid: ${process.pid} is running - isMaster: ${cluster.isMaster}`);
+
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+} else {
+    // Exercise multiply instruction 10 billion times per process
+    for(i=0; i<1e10; i++) {
+        i*i
+    }
+}
+process.exit()
